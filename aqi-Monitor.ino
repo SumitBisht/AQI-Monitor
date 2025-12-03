001200ec9615f83599b073c670856a32a116ce11
sumitbisht <sumitbisht1987@gmail.com>
2025-12-03 16:22:24 +0530

    Added caliberation

--- aqi-Monitor.ino
+++ aqi-Monitor.ino
@@ -4,15 +4,20 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET -1
+// The load resistance on the board
+#define RLOAD 22.0
+// Calibration resistance at atmospheric CO2 level
+#define RZERO 889.13
+
+#define SCREEN_WIDTH 128    // OLED display width, in pixels
+#define SCREEN_HEIGHT 64    // OLED display height, in pixels
+#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup()
 {
   Serial.begin(9600);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with the I2C addr 0x3C (128x64)
   display.clearDisplay();
   delay(10);
 
@@ -35,11 +40,18 @@ void setup()
 }
 
 void loop() {
-    delay(2000);
+    delay(4000);
     MQ135 gasSensor = MQ135(A0);
     float air_quality = gasSensor.getPPM();
+    float zero = gasSensor.getRZero();
+    int val = analogRead(A0); 
+    Serial.print ("raw = "); 
+    Serial.println (val);
+    Serial.print ("rzero: "); 
+    Serial.println (zero); 
     Serial.print("Air Quality (PPM): ");
     Serial.println(air_quality);
+    //// Display this in monitor
     display.clearDisplay();
     display.setCursor(40, 0);
     display.setTextSize(3);
@@ -47,9 +59,8 @@ void loop() {
     display.setCursor(0,30);
     display.setTextSize(2);
     display.setTextColor(WHITE);
-    display.print(air_quality);
+    display.print(val);
     display.setCursor(0,50);
     display.println("PPM (C02)");
     display.display();
-    delay(1000);
 }
